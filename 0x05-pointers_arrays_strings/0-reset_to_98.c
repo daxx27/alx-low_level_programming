d234de65e8b3c08d529986598eedf4d837ea76a6
dagmawi <dagxx27@gmail.com>
2022-03-21 23:23:45 +0300

    first

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include <main.h>
+
+/** this program
+ *
+ * takes a pointer
+ * and store value**/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+/** this
+ * is 
+ * a
+ * comment **/
+void reset_to_98(int *n);
